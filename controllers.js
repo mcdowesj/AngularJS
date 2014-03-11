0f868dabf772db1adca7a3b2dd34051c508836ac
mcdowesj <mcdowesj@tcd.ie>
2014-03-11 13:19:07 +0000

    Update controllers.js
    Tutorial 4 'template'

--- controllers.js
+++ controllers.js
@@ -5,34 +5,15 @@ var phonecatApp = angular.module('phonecatApp', []);
 phonecatApp.controller('PhoneListCtrl', function ($scope) {
 	$scope.phones = [
 	{'name': 'Nexus S',
-	'snippet': 'Fast just got faster with Nexus S.'},
+	'snippet': 'Fast just got faster with Nexus S.',
+	'age': 1},
 	{'name': 'Motorola XOOM™ with Wi-Fi',
-	'snippet': 'The Next, Next Generation tablet.'},
+	'snippet': 'The Next, Next Generation tablet.',
+	'age':2},
 	{'name': 'MOTOROLA XOOM™',
-	'snippet': 'The Next, Next Generation tablet.'},
-	{'name' : 'HTCOne', 
-	'snippet' : 'The best phone in the world'}
+	'snippet': 'The Next, Next Generation tablet.',
+	'age':3}
 	];
+	$scope.orderProp = 'name';
 });
 	
-
-/*
- function TodoCtrl($scope){
-	$scope.totalTodos = 4;
-	
-	$scope.todos = [
-		{text:'Learn AngularJS', done:false}, 
-		{text:'Build an App', done:false}];
- 
- $scope.getLength = function(){
-	return $scope.todos.length;
- };
- 
-	$scope.addTodo = function(){
-		$scope.todos.push({text:$scope.formTodoText, done:false});
-		$scope.formTodoText = '';
-	};
- 
- 
- }
- */
